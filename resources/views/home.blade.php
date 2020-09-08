495c553f07361d75ab8fc727507b1c867a7d8f4b
Fitz-G <fitzgeraldjw04@gmail.com>
2020-09-08 17:58:25 +0000

    Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,31 @@
 @section('content')
     <div class="row">
         <div class="col-md-3">
-            <div class="card text-white bg-primary">
+            <div class="card text-white bg-danger">
+                <div class="card-body">
+                    <blockquote class="card-bodyquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.</p>
+                        <footer class="blockquote-footer text-white-50"> Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-danger">
+                <div class="card-body">
+                    <blockquote class="card-bodyquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.</p>
+                        <footer class="blockquote-footer text-white-50"> Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-danger">
                 <div class="card-body">
                     <blockquote class="card-bodyquote mb-0">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
